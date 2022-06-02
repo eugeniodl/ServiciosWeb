8fcbd0f614120a248ed68aa26103403864473ede
Eugenio Díaz <Eugenio.Diaz@fcys.uni.edu.ni>
2022-06-02 13:29:23 -0600

    Agregar archivos de proyecto.

---
+++ Libreria/LibrosForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Libreria
+{
+    partial class LibrosForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLibros = new System.Windows.Forms.DataGridView();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblISBN = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.lblTemas = new System.Windows.Forms.Label();
+            this.txtTemas = new System.Windows.Forms.TextBox();
+            this.lblEditorial = new System.Windows.Forms.Label();
+            this.txtEditorial = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLibros
+            // 
+            this.dgvLibros.AllowUserToAddRows = false;
+            this.dgvLibros.AllowUserToDeleteRows = false;
+            this.dgvLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLibros.Location = new System.Drawing.Point(24, 190);
+            this.dgvLibros.Name = "dgvLibros";
+            this.dgvLibros.ReadOnly = true;
+            this.dgvLibros.RowTemplate.Height = 24;
+            this.dgvLibros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLibros.Size = new System.Drawing.Size(742, 239);
+            this.dgvLibros.TabIndex = 0;
+            this.dgvLibros.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvLibros_CellClick);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(24, 134);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(118, 35);
+            this.btnRegistrar.TabIndex = 1;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.BtnRegistrar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(234, 134);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(118, 35);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(437, 134);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(118, 35);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(648, 134);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(118, 35);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // lblISBN
+            // 
+            this.lblISBN.AutoSize = true;
+            this.lblISBN.Location = new System.Drawing.Point(31, 45);
+            this.lblISBN.Name = "lblISBN";
+            this.lblISBN.Size = new System.Drawing.Size(39, 17);
+            this.lblISBN.TabIndex = 5;
+            this.lblISBN.Text = "ISBN";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(92, 42);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(194, 22);
+            this.txtISBN.TabIndex = 6;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(320, 45);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(43, 17);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "Título";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(379, 42);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(386, 22);
+            this.txtTitulo.TabIndex = 8;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(31, 88);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(42, 17);
+            this.lblAutor.TabIndex = 9;
+            this.lblAutor.Text = "Autor";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(92, 85);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(160, 22);
+            this.txtAutor.TabIndex = 10;
+            // 
+            // lblTemas
+            // 
+            this.lblTemas.AutoSize = true;
+            this.lblTemas.Location = new System.Drawing.Point(269, 88);
+            this.lblTemas.Name = "lblTemas";
+            this.lblTemas.Size = new System.Drawing.Size(51, 17);
+            this.lblTemas.TabIndex = 11;
+            this.lblTemas.Text = "Temas";
+            // 
+            // txtTemas
+            // 
+            this.txtTemas.Location = new System.Drawing.Point(326, 85);
+            this.txtTemas.Name = "txtTemas";
+            this.txtTemas.Size = new System.Drawing.Size(170, 22);
+            this.txtTemas.TabIndex = 12;
+            // 
+            // lblEditorial
+            // 
+            this.lblEditorial.AutoSize = true;
+            this.lblEditorial.Location = new System.Drawing.Point(512, 88);
+            this.lblEditorial.Name = "lblEditorial";
+            this.lblEditorial.Size = new System.Drawing.Size(59, 17);
+            this.lblEditorial.TabIndex = 13;
+            this.lblEditorial.Text = "Editorial";
+            // 
+            // txtEditorial
+            // 
+            this.txtEditorial.Location = new System.Drawing.Point(577, 85);
+            this.txtEditorial.Name = "txtEditorial";
+            this.txtEditorial.Size = new System.Drawing.Size(188, 22);
+            this.txtEditorial.TabIndex = 14;
+            // 
+            // LibrosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEditorial);
+            this.Controls.Add(this.lblEditorial);
+            this.Controls.Add(this.txtTemas);
+            this.Controls.Add(this.lblTemas);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.lblAutor);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.lblISBN);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.dgvLibros);
+            this.Name = "LibrosForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.LibrosForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLibros;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblISBN;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label lblTemas;
+        private System.Windows.Forms.TextBox txtTemas;
+        private System.Windows.Forms.Label lblEditorial;
+        private System.Windows.Forms.TextBox txtEditorial;
+    }
+}
+
---
+++ Libreria/LibrosForm.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
+{
+    public partial class LibrosForm : Form
+    {
+        private static int id = 0;
+        public LibrosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LibrosForm_Load(object sender, EventArgs e)
+        {
+            GetAllLibros();
+        }
+
+        private async void GetAllLibros()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("http://localhost:49795/api/Libros"))
+                {
+                    if(response.IsSuccessStatusCode)
+                    {
+                        var LibroJsonString = await response.Content.ReadAsStringAsync();
+                        dgvLibros.DataSource = JsonConvert.DeserializeObject<List<ViewModels.LibrosViewModel>>(LibroJsonString).ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se puede obtener el libro: " + response.StatusCode);
+                    }
+                }
+            }
+        }
+
+        private async void GetLibroById(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                string URI = "http://localhost:49795/api/Libros/?id=" + id.ToString();
+                HttpResponseMessage response = await client.GetAsync(URI);
+                if(response.IsSuccessStatusCode)
+                {
+                    var LibroJsonString = await response.Content.ReadAsStringAsync();
+                    ViewModels.LibrosViewModel oLibro = JsonConvert.DeserializeObject<ViewModels.LibrosViewModel>(LibroJsonString);
+                    txtISBN.Text = oLibro.ISBN;
+                    txtTitulo.Text = oLibro.Titulo;
+                    txtAutor.Text = oLibro.Autor;
+                    txtTemas.Text = oLibro.Temas;
+                    txtEditorial.Text = oLibro.Editorial;
+                }
+                else
+                {
+                    MessageBox.Show("No se puede obtener el libro: " + response.StatusCode);
+                }
+            }
+        }
+
+        private async void AddLibro()
+        {
+            ViewModels.LibrosViewModel oLibro = new ViewModels.LibrosViewModel();
+            oLibro.ISBN = txtISBN.Text;
+            oLibro.Titulo = txtTitulo.Text;
+            oLibro.Autor = txtAutor.Text;
+            oLibro.Temas = txtTemas.Text;
+            oLibro.Editorial = txtEditorial.Text;
+            using (var client = new HttpClient())
+            {
+                var serializedLibro = JsonConvert.SerializeObject(oLibro);
+                var content = new StringContent(serializedLibro, Encoding.UTF8, "application/json");
+                var result = await client.PostAsync("http://localhost:49795/api/Libros", content);
+            }
+            Limpiar();
+            GetAllLibros();
+        }
+
+        private async void UpdateLibro(int id)
+        {
+            ViewModels.LibrosViewModel oLibro = new ViewModels.LibrosViewModel();
+            oLibro.Id = id;
+            oLibro.ISBN = txtISBN.Text;
+            oLibro.Titulo = txtTitulo.Text;
+            oLibro.Autor = txtAutor.Text;
+            oLibro.Temas = txtTemas.Text;
+            oLibro.Editorial = txtEditorial.Text;
+
+            using (var client = new HttpClient())
+            {
+                var serializedLibro = JsonConvert.SerializeObject(oLibro);
+                var content = new StringContent(serializedLibro, Encoding.UTF8, "application/json");
+                HttpResponseMessage responseMessage = await client.PutAsync("http://localhost:49795/api/Libros/?id=" + oLibro.Id, content);
+                if (responseMessage.IsSuccessStatusCode)
+                    MessageBox.Show("Libro actualizado");
+                else
+                    MessageBox.Show("Error al actualizar el libro: " + responseMessage.StatusCode);
+            }
+            Limpiar();
+            GetAllLibros();
+        }
+
+        private async void DeleteLibro(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49795/api/Libros");
+                HttpResponseMessage responseMessage = await client.DeleteAsync(String.Format("{0}/{1}", "http://localhost:49795/api/Libros", id));
+                if (responseMessage.IsSuccessStatusCode)
+                    MessageBox.Show("Libro eliminado con éxito");
+                else
+                    MessageBox.Show("No se pudo eliminar el libro: " + responseMessage.StatusCode);
+            }
+            Limpiar();
+            GetAllLibros();
+        }
+
+        private void Limpiar()
+        {
+            txtISBN.Text = String.Empty;
+            txtTitulo.Text = String.Empty;
+            txtAutor.Text = String.Empty;
+            txtTemas.Text = String.Empty;
+            txtEditorial.Text = String.Empty;
+            id = 0;
+        }
+
+        private void DgvLibros_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach(DataGridViewRow row in dgvLibros.Rows)
+            {
+                if(row.Index == e.RowIndex)
+                {
+                    id = int.Parse(row.Cells[0].Value.ToString());
+                    GetLibroById(id);
+                }
+            }
+        }
+
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            AddLibro();
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            if (id != 0)
+                UpdateLibro(id);
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            if (id != 0)
+                DeleteLibro(id);
+        }
+    }
+}
